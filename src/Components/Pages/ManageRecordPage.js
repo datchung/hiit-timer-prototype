361cb35cf6093021088b3ec3ececdd988e045236
Dat Chung <datchung@users.noreply.github.com>
2019-10-20 23:30:46 -0400

    Move back button

--- src/Components/Pages/ManageRecordPage.js
+++ src/Components/Pages/ManageRecordPage.js
@@ -16,16 +16,17 @@ function ManageRecordPage(props) {
     <React.Fragment>
       <div className="columns is-mobile">
         <div className="column">
-          <h4 className="title is-4">{subTitle}</h4>
-        </div>
-      </div>
-      <div className="columns is-mobile">
-        <div className="column">
           <Link to="/">
             <button className="button">{T.t("back")}</button>
           </Link>
         </div>
       </div>
+      <div className="columns is-mobile">
+        <div className="column">
+          <h4 className="title is-4">{subTitle}</h4>
+        </div>
+      </div>
+
       <ManageRecord {...props} />
     </React.Fragment>
   );
